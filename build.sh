cb25e53d6dcaaa13012afabd8429ceea278fdc07
North <YangShi2@sf-express.com>
2018-08-21 14:44:23 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -15,10 +15,11 @@ fi
 rm -rf ./output
 
 mkdir -p ./output/webroot/static
-mkdir -p ./output/webroot/crmanage
+mkdir -p ./output/webroot/app
 #mkdir -p ./output/webroot/404
 
-# SaaS 代理商端
-cp ./build/index.html ./output/webroot/crmanage/index.php
+# ReactMobx
+cp ./build/index.html ./output/webroot/app/index.php
+cp ./build/favicon.ico ./output/webroot/favicon.ico
 #cp ./build/404.html ./output/webroot/404/index.php
 cp -r ./build/static/* ./output/webroot/static
