79890545edc8fa0f94f0994c38725618765f0bdc
babyisun <babyisun@qq.com>
2018-09-07 16:56:50 +0800

    upgrade yarn

--- build.sh
+++ build.sh
@@ -2,11 +2,18 @@
 
 echo 'node \c'
 node -v
-echo 'npm \c'
-npm -v
 
-npm install
-npm run build
+if which yarn 2>/dev/null; then
+    echo 'yarn \c'
+    yarn -v
+    yarn
+    yarn build
+else
+    echo 'npm \c'
+    npm -v
+    npm install
+    npm run build
+fi
 
 if [ $? -ne 0 ]; then
 exit 1
@@ -15,11 +22,11 @@ fi
 rm -rf ./output
 
 mkdir -p ./output/webroot/static
-mkdir -p ./output/webroot/app
+mkdir -p ./output/webroot/web
 #mkdir -p ./output/webroot/404
 
 # ReactMobx
-cp ./build/index.html ./output/webroot/app/index.php
+cp ./build/index.html ./output/webroot/web/index.php
 cp ./build/favicon.ico ./output/webroot/favicon.ico
 #cp ./build/404.html ./output/webroot/404/index.php
 cp -r ./build/static/* ./output/webroot/static
