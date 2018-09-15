8a1d681d71cd5902926b5b6a0dfd944aa9c20490
haoli <673252102@outlook.com>
2018-09-15 16:07:33 +0800

    添加enzyme依赖，并传入相关函数使其成功通过测试

--- test/App.test.js
+++ test/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/App';
 
+// UI是否正常渲染
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
--- test/pages/One/TwoA/index.test.js
+++ test/pages/One/TwoA/index.test.js
@@ -1,10 +1,20 @@
 // 对One/TwoA页面主页dom进行测试
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import TwoA from '@/pages/One/TwoA/index.jsx';
 
+
+configure({ adapter: new Adapter() });
+
+const props = {
+  stores: {
+    twoA: {
+      load: jest.fn(),
+    },
+  },
+};
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TwoA />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<TwoA {...props} />);
 });
