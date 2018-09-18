3b95112634cd4405fd84b7911f300d1dea9501de
haoli <673252102@outlook.com>
2018-09-18 19:17:02 +0800

    去掉项目里的服务端渲染代码

--- server/ignore.js
+++
@@ -1,8 +0,0 @@
-// 该文件用于忽略那些文件中不需要加载的less，scss等文件
-const ignore = () => {
-  const extensions = ['.css', '.scss', '.less', '.png', '.jpg', '.gif'];
-  for (let i = 0, len = extensions.length; i < len; i++) {
-    require.extensions[extensions[i]] = () => false;
-  }
-};
-module.exports = ignore;
--- server/routers/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('koa-router')();
-const { render } = require('../render/index');
-
-router.get('/', render);
-
-module.exports = router;
